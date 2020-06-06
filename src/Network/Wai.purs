a5511f36b3e5dc70ff388df83dc15b8361be6818
Woodson Delhia <woodsondelhia88@gmail.com>
2020-06-06 21:20:12 +0900

    Parametrized Application, and constrained parameter to MonadEffect

--- src/Network/Wai.purs
+++ src/Network/Wai.purs
@@ -15,6 +15,7 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap)
 import Effect (Effect)
+import Effect.Class (class MonadEffect)
 import Foreign.Object as Object
 import Network.HTTP.Types (Status, ResponseHeaders)
 import Network.HTTP.Types as H
@@ -24,15 +25,19 @@ import Node.Net.Socket as Net
 import Node.Stream as Stream
 import Unsafe.Coerce (unsafeCoerce)
 
-type Application = Request -> (Response -> Effect Unit) -> Effect Unit
+-- | The reason for using MonadEffect as a contraint is to 
+-- | have the user control how they want their logic to run sequential/parallel. 
+-- | This approach forces the user to use function such as `launchAff` in order to have their logic 
+-- | run in parallel 
+type Application m = forall m. MonadEffect m => Request -> (Response -> m Unit) -> m Unit
 
-type Middleware = Application -> Application
+type Middleware m = Application m -> Application m
 
--- TO-CONSIDER:
--- Need to expose the node Response in a case where 
--- we need to process something that requires HttpIncomingRequest 
--- from http lib. However, It would be preferrable to have that handle be polymorphic
--- in a situation where we would like to support http2. Maybe existential type can help us here?
+-- | TO-CONSIDER:
+-- | Need to expose the node Response in a case where 
+-- | we need to process something that requires HttpIncomingRequest 
+-- | from http lib. However, It would be preferrable to have that handle be polymorphic
+-- | in a situation where we would like to support http2. Maybe existential type can help us here?
 defaultRequest :: Request
 defaultRequest = 
     Request { method: H.GET
